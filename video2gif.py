696622e72bbfb90c4e850d59a8b2ac158ee279ed
abstract-enough <77987885+abstract-enough@users.noreply.github.com>
2026-04-28 10:00:15 -0400

    Add files via upload

---
+++ video2gif.py
@@ -0,0 +1,25 @@
+﻿import cv2
+import imageio
+
+
+cap = cv2.VideoCapture("/content/drive/MyDrive/Nick_G_3_6_D.mp4")
+
+
+gif_frames = []
+
+
+while True:
+
+
+    ret, frame = cap.read()
+    if ret:
+      color_frame = cv2.cvtColor(frame,
+cv2.COLOR_BGR2RGB)
+      gif_frames.append(color_frame)
+
+
+    if not ret:
+        break
+
+
+imageio.mimsave("/content/drive/MyDrive/Nick_G_3_6_D.gif", gif_frames, fps=10)
